dc4873f83123d7a50aa826cdf61e3f11fe426442
felixyeahh <felixyeah@outlook.com>
2023-02-28 14:31:36 -0800

    👌 IMPROVE: add parse mode to bot

--- jivopolis-alt/bot.py
+++ jivopolis-alt/bot.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher
 from .config import TOKEN
 
-bot = Bot(token=TOKEN)
+bot = Bot(token=TOKEN, parse_mode='html', disable_web_page_preview=True)
 dp = Dispatcher(bot)
