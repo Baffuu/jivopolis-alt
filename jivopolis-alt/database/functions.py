25540a2088f05c7626dbad731af1ff87d70ce8c0
Mikita from Baffu <aniki.malahov081@gmail.com>
2023-08-05 10:37:30 +0300

    Remove some useless exception handling

--- jivopolis-alt/database/functions.py
+++ jivopolis-alt/database/functions.py
@@ -392,121 +392,105 @@ async def achieve(user_id: int | str, chat_id : int | str, achievement: str) -> 
     :param chat_id (int) - Telegram Chat ID of chat in which messages will be sent 
     :param achievement (str) - Index of achievement 
     """
-    try:
-        achieve = cur.execute(f"SELECT {achievement} FROM userdata WHERE user_id={user_id}").fetchone()
-        
-        if achieve != 0:
-            return
+    achieve = cur.execute(f"SELECT {achievement} FROM userdata WHERE user_id={user_id}").fetchone()
+    
+    if achieve != 0:
+        return
 
-        index = ach[0].index(achievement)
-        name = ach[1][index]
-        desc = ach[2][index]
-        money = ach[3][index]
-        points = ach[4][index]
+    index = ach[0].index(achievement)
+    name = ach[1][index]
+    desc = ach[2][index]
+    money = ach[3][index]
+    points = ach[4][index]
 
-        cur.execute(f"UPDATE userdata SET {achievement} = 1 WHERE user_id = {user_id}")
-        conn.commit()
+    cur.execute(f"UPDATE userdata SET {achievement} = 1 WHERE user_id = {user_id}")
+    conn.commit()
 
-        rasa = cur.execute(f"SELECT rasa FROM userdata WHERE user_id = {user_id}").fetchone()
-        nick = cur.execute(f"SELECT nick FROM userdata WHERE user_id = {user_id}").fetchone()
+    rasa = cur.execute(f"SELECT rasa FROM userdata WHERE user_id = {user_id}").fetchone()
+    nick = cur.execute(f"SELECT nick FROM userdata WHERE user_id = {user_id}").fetchone()
 
-        cur.execute(f"UPDATE userdata SET balance = balance + {money} WHERE user_id = {user_id}")
-        conn.commit()
-        cur.execute(f"UPDATE userdata SET points = points+{points} WHERE user_id = {user_id}")
-        conn.commit()
+    cur.execute(f"UPDATE userdata SET balance = balance + {money} WHERE user_id = {user_id}")
+    conn.commit()
+    cur.execute(f"UPDATE userdata SET points = points+{points} WHERE user_id = {user_id}")
+    conn.commit()
 
-        chat_type = await bot.get_chat(chat_id)
-        chat_type = chat_type.type
+    chat_type = await bot.get_chat(chat_id)
+    chat_type = chat_type.type
 
-        if chat_type == "private":
-            await bot.send_message(chat_id, f"<i>У вас новое достижение: <b>{name}</b>\n{desc}. \nВаша награда: <b>${money}</b> и &#128161; <b>{points}</b> очков</i>")
-        else:
-            await bot.send_message(chat_id, f"<i><b><a href=\"tg://user?id={user_id}\">{rasa}{nick}</a></b>, у вас новое достижение: <b>{name}</b>\n{desc}. \nВаша награда: <b>${money}</b> и &#128161; <b>{points}</b> очков</i>")
-    except Exception as e:
-        await bot.send_message(
-            chat_id, 
-            (
-                "&#10060; <i>При выполнении команды произошла ошибка. Проверьте, есть ли у вас аккаунт в Живополисе. "
-                "Если вы выполняли действие над другим пользователем, проверьте, есть ли у этого пользователя аккаунт"
-                " в Живополисе. Помните, что выполнение действий над ботом Живополиса невозможно.\nЕсли ошибка появля"
-                "ется даже когда у вас есть аккаунт, возможно, проблема в коде Живополиса. Сообщите о ней в Приёмную "
-                "(t.me/zhivolab), и мы постараемся исправить проблему.\nИзвините за предоставленные неудобства</i>"
-            ))
-        await bot.send_message(chat_id, f"<i><b>Текст ошибки: </b>{e}</i>")
+    if chat_type == "private":
+        await bot.send_message(chat_id, f"<i>У вас новое достижение: <b>{name}</b>\n{desc}. \nВаша награда: <b>${money}</b> и &#128161; <b>{points}</b> очков</i>")
+    else:
+        await bot.send_message(chat_id, f"<i><b><a href=\"tg://user?id={user_id}\">{rasa}{nick}</a></b>, у вас новое достижение: <b>{name}</b>\n{desc}. \nВаша награда: <b>${money}</b> и &#128161; <b>{points}</b> очков</i>")
 
 
-async def cure(user_id: str, target_id: str, chat_id: str) -> None | Message: #function is useless now...
+async def cure(user_id: str, target_id: str, chat_id: str) -> None | Message: # function is useless now...
     '''
     to cure someone...
     '''
-    try:
-        nerr = 0
-        medicine = cur.execute(f"SELECT medicine FROM userdata WHERE user_id={user_id}").fetchone()
+    nerr = 0
+    medicine = cur.execute(f"SELECT medicine FROM userdata WHERE user_id={user_id}").fetchone()
 
-        if medicine < 1:
-            return await bot.send_message(chat_id, "<i>&#10060; У вас нет таблеток(</i>")
+    if medicine < 1:
+        return await bot.send_message(chat_id, "<i>&#10060; У вас нет таблеток(</i>")
 
-        health = cur.execute(f"SELECT health FROM userdata WHERE user_id={target_id}").fetchone()
+    health = cur.execute(f"SELECT health FROM userdata WHERE user_id={target_id}").fetchone()
 
-        nick = cur.execute(f"SELECT nick FROM userdata WHERE user_id={user_id}").fetchone()
-        mask = get_mask(user_id)
+    nick = cur.execute(f"SELECT nick FROM userdata WHERE user_id={user_id}").fetchone()
+    mask = get_mask(user_id)
 
-        target_nick = cur.execute(f"SELECT nick FROM userdata WHERE user_id={target_id}").fetchone()
-        target_mask = get_mask(target_id)
+    target_nick = cur.execute(f"SELECT nick FROM userdata WHERE user_id={target_id}").fetchone()
+    target_mask = get_mask(target_id)
 
-        if health > 0 and health < 100:
-            cur.execute(f"UPDATE userdata SET medicine=medicine-1 WHERE user_id={user_id}")
+    if health > 0 and health < 100:
+        cur.execute(f"UPDATE userdata SET medicine=medicine-1 WHERE user_id={user_id}")
+        conn.commit()
+
+        rand = random.randint(1, 100-health)
+
+        cur.execute(f"UPDATE userdata SET health=health+{rand} WHERE user_id={target_id}")
+        conn.commit()
+
+        if target_id == user_id:
+            return await bot.send_message(chat_id, "<i>&#128138; Успех! Вы вылечили себя</i>")
+        await bot.send_message(chat_id, f"<i>&#128138; Успех! Вы вылечили <b><a href=\"{await get_link(target_id)}\">{target_mask}{target_nick}</a></b></i>")
+        await bot.send_message(target_id, f"<i>&#128138; Вас вылечил <b><a href=\"{await get_link(user_id)}\">{mask}{nick}</a></b></i>")
+        nerr = 1
+
+    elif health >= 100:
+        if target_id != user_id:
+            return await bot.send_message(chat_id, f"<i>&#128138; <b><a href=\"{await get_link(target_id)}\">{target_mask}{target_nick}</a></b> полностью здоров, зачем вам тратить лекарства впустую?\nЛекарства <b>не потрачены</b></i>")   
+        else:                                   
+            return await bot.send_message(chat_id, f"<i>&#128138; Вы полностью здоровы, зачем вам тратить лекарства впустую?\nЛекарства <b>не потрачены</b></i>")
+    else:
+        if target_id == user_id:
+            return await bot.send_message(chat_id, "<i>&#10060; Вы не можете воскресить самого себя</i>")
+
+        cur.execute(f"UPDATE userdata SET medicine=medicine-1 WHERE user_id={user_id}")
+        conn.commit()
+
+        rand = random.randint(50,100)
+
+        cur.execute(f"UPDATE userdata SET health={rand} WHERE user_id={target_id}")
+        conn.commit()
+
+        await bot.send_message(chat_id, f"<i>&#128138; Успех! Вы воскресили <b><a href=\"{await get_link(target_id)}\">{target_mask}{target_nick}</a></b></i>")
+        nerr = 1
+        await bot.send_message(target_id, f"<i>&#128138; Вас воскресил <b><a href=\"{await get_link(user_id)}\">{mask}{nick}</a></b></i>")
+
+        await achieve(user_id, chat_id, "helper")
+
+    if nerr == 1:
+        cur.execute(f"UPDATE userdata SET cured=cured+1 WHERE user_id={user_id}")
+        conn.commit()
+
+        cured = cur.execute(f"SELECT cured FROM userdata WHERE user_id={user_id}").fetchone()
+
+        if cured >= 20:
+            await achieve(user_id, chat_id, "medquest")
+            cur.execute(f"UPDATE userdata SET medic=medic+1 WHERE user_id={user_id}")
             conn.commit()
+            await bot.send_message(chat_id, "<i>Вы получаете <b>🩺 Стетоскоп</b>. Эта маска будет показывать всем, что вы профессиональный врач, и вам можно доверять</i>")
 
-            rand = random.randint(1, 100-health)
-
-            cur.execute(f"UPDATE userdata SET health=health+{rand} WHERE user_id={target_id}")
-            conn.commit()
-
-            if target_id == user_id:
-                return await bot.send_message(chat_id, "<i>&#128138; Успех! Вы вылечили себя</i>")
-            await bot.send_message(chat_id, f"<i>&#128138; Успех! Вы вылечили <b><a href=\"{await get_link(target_id)}\">{target_mask}{target_nick}</a></b></i>")
-            await bot.send_message(target_id, f"<i>&#128138; Вас вылечил <b><a href=\"{await get_link(user_id)}\">{mask}{nick}</a></b></i>")
-            nerr = 1
-
-        elif health >= 100:
-            if target_id != user_id:
-                return await bot.send_message(chat_id, f"<i>&#128138; <b><a href=\"{await get_link(target_id)}\">{target_mask}{target_nick}</a></b> полностью здоров, зачем вам тратить лекарства впустую?\nЛекарства <b>не потрачены</b></i>")   
-            else:                                   
-                return await bot.send_message(chat_id, f"<i>&#128138; Вы полностью здоровы, зачем вам тратить лекарства впустую?\nЛекарства <b>не потрачены</b></i>")
-        else:
-            if target_id == user_id:
-                return await bot.send_message(chat_id, "<i>&#10060; Вы не можете воскресить самого себя</i>")
-
-            cur.execute(f"UPDATE userdata SET medicine=medicine-1 WHERE user_id={user_id}")
-            conn.commit()
-
-            rand = random.randint(50,100)
-
-            cur.execute(f"UPDATE userdata SET health={rand} WHERE user_id={target_id}")
-            conn.commit()
-
-            await bot.send_message(chat_id, f"<i>&#128138; Успех! Вы воскресили <b><a href=\"{await get_link(target_id)}\">{target_mask}{target_nick}</a></b></i>")
-            nerr = 1
-            await bot.send_message(target_id, f"<i>&#128138; Вас воскресил <b><a href=\"{await get_link(user_id)}\">{mask}{nick}</a></b></i>")
-
-            await achieve(user_id, chat_id, "helper")
-
-        if nerr == 1:
-            cur.execute(f"UPDATE userdata SET cured=cured+1 WHERE user_id={user_id}")
-            conn.commit()
-
-            cured = cur.execute(f"SELECT cured FROM userdata WHERE user_id={user_id}").fetchone()
-
-            if cured >= 20:
-                await achieve(user_id, chat_id, "medquest")
-                cur.execute(f"UPDATE userdata SET medic=medic+1 WHERE user_id={user_id}")
-                conn.commit()
-                await bot.send_message(chat_id, "<i>Вы получаете <b>🩺 Стетоскоп</b>. Эта маска будет показывать всем, что вы профессиональный врач, и вам можно доверять</i>")
-
-    except Exception as e:
-        await bot.send_message(chat_id, "&#10060; <i>При выполнении команды произошла ошибка. Проверьте, есть ли у вас аккаунт в Живополисе. Если вы выполняли действие над другим пользователем, проверьте, есть ли у этого пользователя аккаунт в Живополисе. Помните, что выполнение действий над ботом Живополиса невозможно.\nЕсли ошибка появляется даже когда у вас есть аккаунт, возможно, проблема в коде Живополиса. Сообщите о ней в Приёмную (t.me/zhivolab), и мы постараемся исправить проблему.\nИзвините за предоставленные неудобства</i>")
-        await bot.send_message(chat_id, f"<i><b>Текст ошибки: </b>{e}</i>")
 
 class profile_():
     def __init__(self, dont_init: bool = False, user_id: Optional[int] = None, message: Optional[Message] = None, called: bool = False) -> None:
